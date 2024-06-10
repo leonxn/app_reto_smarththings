7f8bf1dae641ae635331c503c437eaea7fde1354
carlos leon <cleon050591@gmail.com>
2024-06-10 09:05:18 -0500

    Guardar cambios locales

---
+++ lib/pages/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        children: [
+          Text("prueba"),
+          // Image.asset('assets/images/wallpapers/wallpaperLogin.png'),
+          // Container(
+          //   decoration: BoxDecoration(),
+          // ),
+        ],
+      ),
+    );
+  }
+}
